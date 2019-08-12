0813a63538a31ad1d0d6494cea1f1ec530259d38
craken-dev <craken.dev@gmail.com>
2019-08-12 17:57:28 +0200

    added color switch

---
+++ Color Switch Replica (CSR)/Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float jump_force = 10f;
+
+    private Rigidbody2D rb;
+
+    private string color;
+    [SerializeField] Color blue;
+    [SerializeField] Color yellow;
+    [SerializeField] Color pink;
+    [SerializeField] Color purple;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        GetRandomColor();
+        AssignColor();
+        Debug.Log("YOUR COLOR IS " + color.ToString());
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(new Vector2(0, jump_force));
+        }
+    }
+    private void GetRandomColor()
+    {
+        int index = Random.Range(0, 3);
+
+        switch (index)
+        {
+            case 0:
+                color = "blue";
+                break;
+            case 1:
+                color = "yellow";
+                break;
+            case 2:
+                color = "pink";
+                break;
+            case 3:
+                color = "purple";
+                break;
+            default:
+                Debug.LogError("index inespected");
+                break;
+        }
+    }
+    void AssignColor()
+    {
+        if(color == "blue")
+        {
+            GetComponent<SpriteRenderer>().color = blue;
+        }else if(color == "yellow")
+        {
+            GetComponent<SpriteRenderer>().color = yellow;
+        }
+        else if(color == "pink")
+        {
+            GetComponent<SpriteRenderer>().color = pink;
+        }
+        else if(color == "purple")
+        {
+            GetComponent<SpriteRenderer>().color = purple;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag == color)
+        {
+            Debug.Log("YEEE");
+        }
+        else
+        {
+            Debug.Log("NOOO");
+        }
+    }
+}
---
+++ Color Switch Replica (CSR)/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private float rotation_speed = 100f;
+    
+    private void Update()
+    {
+        transform.Rotate(0f, 0f, rotation_speed * Time.deltaTime);
+    }
+}
